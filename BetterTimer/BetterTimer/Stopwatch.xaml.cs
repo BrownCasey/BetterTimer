e2fe2a96193f45b46bef59955d53184947250792
Casey <cbro234@gmail.com>
2019-04-05 06:51:49 -0700

    Timer and Stopwatch TimeSpans elapse time

--- BetterTimer/BetterTimer/Stopwatch.xaml.cs
+++ BetterTimer/BetterTimer/Stopwatch.xaml.cs
@@ -12,9 +12,43 @@ namespace BetterTimer
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Stopwatch : ContentPage
 	{
+        public System.Timers.Timer swTimer;
+        public TimeSpan counter { get; set; }
+        public TimeSpan tenthSecond = new TimeSpan(1000000);
 		public Stopwatch ()
 		{
+            counter = new TimeSpan(0);
+            swTimer = new System.Timers.Timer();
+            swTimer.Interval = 100;
+            swTimer.Elapsed += TimerLoop;
 			InitializeComponent ();
 		}
-	}
+        
+        public void OnStart(object sender, EventArgs e)
+        {
+            swTimer.Enabled = true;
+            swTimer.Start();
+        }
+        public void OnPause(object sender, EventArgs e)
+        {
+            swTimer.Stop();
+        }
+        public void OnReset(object sender, EventArgs e)
+        {
+            AlertMe("Stopwatch Reset");
+            counter = new TimeSpan(0);
+            Xamarin.Forms.Device.BeginInvokeOnMainThread(() => ClockDisplay.Text = counter.ToString(@"hh\:mm\:ss\:f"));
+        }
+
+        private void TimerLoop(object sender, EventArgs e)
+        {
+            counter += tenthSecond;
+            Xamarin.Forms.Device.BeginInvokeOnMainThread(() => ClockDisplay.Text = counter.ToString(@"hh\:mm\:ss\:f"));
+        }
+
+        async void AlertMe(string s)
+        {
+            await DisplayAlert("Stopwatch", s, "OK");
+        }
+    }
 }
--- BetterTimer/BetterTimer/Timer.xaml.cs
+++ BetterTimer/BetterTimer/Timer.xaml.cs
@@ -12,9 +12,42 @@ namespace BetterTimer
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Timer : ContentPage
 	{
+        public System.Timers.Timer cdTimer = new System.Timers.Timer();
+        public TimeSpan countdown;
+        public TimeSpan tenthSecond = new TimeSpan(1000000);
 		public Timer ()
 		{
+            countdown = new TimeSpan(100000000);
+            cdTimer.Interval = 100;
+            cdTimer.Elapsed += TimerElapsed;
 			InitializeComponent ();
 		}
-	}
+
+        private void OnStart(object sender, EventArgs e)
+        {
+            cdTimer.Enabled = true;
+            cdTimer.Start();
+        }
+
+        public void TimerElapsed(object sender, EventArgs e)
+        {
+            if (countdown < tenthSecond)
+            {
+                CountdownDisplay.Text = tenthSecond.ToString(@"hh\:mm\:ss\:ff");
+                cdTimer.Stop();
+                AlertMe("Countdown Ended");
+            }
+            else
+            {
+                countdown -= tenthSecond;
+                Xamarin.Forms.Device.BeginInvokeOnMainThread(() => CountdownDisplay.Text = countdown.ToString(@"hh\:mm\:ss\:ff"));
+            }
+        }
+
+        // This alert doesn't appear, closes app.
+        async void AlertMe(string s)
+        {
+            await DisplayAlert("Countdown", s, "OK");
+        }
+    }
 }
