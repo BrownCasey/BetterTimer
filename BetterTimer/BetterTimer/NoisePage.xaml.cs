e04fee76bd2229c650c1e7fac2dfae21d441eab3
Casey <cbro234@gmail.com>
2019-04-08 06:27:11 -0700

    Playing sounds via Plugin.SimpleAudioPlayer

---
+++ BetterTimer/BetterTimer/NoisePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BetterTimer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NoisePage : ContentPage
+	{
+        //Xam.Plugin.SimpleAudioPlayer
+        //from-the-docs-https://devblogs.microsoft.com/xamarin/adding-sound-xamarin-forms-app
+        public NoisePage ()
+		{
+			InitializeComponent ();
+		}
+
+        public void PlaySound(object sender, EventArgs e)
+        {
+
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream audioStream = assembly.GetManifestResourceStream("BetterTimer.Audio.Alarm09.wav");
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            player.Load(audioStream);
+            player.Play();
+        }
+	}
+}
