7a1f7177d3ca5878bcc4ca00ce6336b9193aadc2
Casey <cbro234@gmail.com>
2019-04-14 14:22:51 -0700

    Android Db

---
+++ BetterTimer/BetterTimer.Android/DatabaseConnectionAndroid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SQLite;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BetterTimer.Droid
+{
+    class DatabaseConnectionAndroid : IDatabaseConnection
+    {
+        public SQLiteConnection DbConnection()
+        {
+            var dbName = "AlarmsDb.db3";
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            path = System.IO.Path.Combine(path, dbName);
+            return new SQLiteConnection(path);
+        }
+    }
+}
